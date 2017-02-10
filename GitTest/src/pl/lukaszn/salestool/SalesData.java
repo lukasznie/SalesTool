14f60c6e1b77a45f6db1537c2fb14b2f24c77568
lukasz <lukasz.nieradko88@gmail.com>
2017-02-10 19:58:15 +0100

    Initial checkin of sales tool.

---
+++ GitTest/src/pl/lukaszn/salestool/SalesData.java
@@ -0,0 +1,9 @@
+package pl.lukaszn.salestool;
+
+public class SalesData {
+	private int[] data = {0, 4, 42};
+	
+	public void display(){
+		System.out.println("TODO: Implement data display!");
+	}
+}
